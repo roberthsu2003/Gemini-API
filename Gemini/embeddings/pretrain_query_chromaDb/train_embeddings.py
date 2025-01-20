8f97ba59d79c8b82063e78d0a16e8655a5fced8f
roberthsu2003 <roberthsu2003@gmail.com>
2025-01-20 04:57:23 +0000

    modify

--- Gemini/embeddings/pretrain_query_chromaDb/train_embeddings.py
+++ Gemini/embeddings/pretrain_query_chromaDb/train_embeddings.py
@@ -1,10 +1,19 @@
 import pandas as pd
 from document_search import DocumentSearch
-from pathlib import Path
+import chromadb
 
 def train_and_save_embeddings():
     # Initialize document search
-    doc_search = DocumentSearch() 
+    doc_search = DocumentSearch()
+
+    # 初始化ChromaDb,建立永久儲存
+    client = chromadb.PersistentClient()
+    
+    collection = client.get_or_create_collection(
+        name="charging_station",
+        metadata={"description": "充電站位置資訊"}
+    )
+    print("Created new collection")
 
     df = pd.read_csv('001.csv')
     df['說明'] = '地址:' + df['地址']+',' + \
@@ -19,12 +28,28 @@ def train_and_save_embeddings():
     # 创建DataFrame
     df = result_df.copy()       
 
+    #準備要加入ChromaDb的資料
     # Generate embeddings
-    df['Embeddings'] = doc_search.create_document_embeddings(df)
-    
-    # Save DataFrame
-    df.to_pickle("embeddings.pkl")
-    print("Embeddings saved successfully")
+    embeddings = doc_search.create_document_embeddings(df)
+    print(embeddings)
+    documents= df['Text'].tolist()
+    metadatas =[{'title': title} for title in df['Title'].tolist()]
+    ids = [str(i) for i in range(len(documents))]
+
+    #將資料加入ChromaDb
+    collection.add(
+        documents=documents,
+        embeddings=embeddings,
+        metadatas=metadatas,
+        ids=ids
+    )
+
+    #檢查是否成功加入   
+    print(collection.peek())
+    print("資料已成功儲存至ChromaDb")
+
+
+
 
 if __name__ == "__main__":
     train_and_save_embeddings()
